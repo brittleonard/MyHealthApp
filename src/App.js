258c1788b4c77e2a27f5af71b913dafeedc5f3f7
brittanyleonard <Belah615@gmail.com>
2021-02-24 14:22:40 -0600

    create post is working

--- src/App.js
+++ src/App.js
@@ -5,80 +5,55 @@ import './App.css';
 import axios from 'axios'
 
 class App extends Component {
-   constructor(props) {
-      super(props)
-      this.state = {
-        posts: [],
+   // constructor(props) {
+   //    super(props)
+      state = {
+         name: '',
+         date: '',
+         details: '',
+         image: '',
+         posts: [],
       }
+
+
+
+   handleChange = (event) => {
+      this.setState({
+         [event.target.id]: event.target.value,
+      })
+   }
+
+  getPosts = () => {
+      axios.get('https://womenshealth-backend.herokuapp.com/posts')
+      .then((response) => this.setState({posts: response.data}))
+      .catch((error) => console.error(error))
+}
+
+ addPost = (event) => {
+    axios.post('https://womenshealth-backend.herokuapp.com/posts',
+    this.state).then((response) => {this.getPosts()})
+
+}
+
+   deletePost = (event, id) => {
+      event.preventDefault();
+      axios.delete('https://womenshealth-backend.herokuapp.com/posts/' + event.target.value).then((response) => {this.getPosts()})
    }
 
 
-  handleChange = (event) => {
-    this.setState({
-      [event.target.id]: event.target.value,
-    })
+  componentDidMount = () => {
+    this.getPosts()
   }
 
- addPost = (event) => {
-    event.preventDefault();
-    axios.post('https://womenshealth-backend.herokuapp.com/posts', {
-      name: this.state.name,
-      date: this.state.date,
-      details: this.state.details,
-      image: this.state.image
-   }).then((response) => {
-      console.log(this.state.posts);
-      console.log(response.data);
-      this.setState({
-         posts: response.data
-      })
-   }).catch((error) => console.log(error))
-}
 
-  deletePost = (event, id) => {
-    axios.delete('https://womenshealth-backend.herokuapp.com/posts/' + event.target.value).then((response) => {
-      // this.getPosts()
-      const post = this.state.posts.filter((post) => post.id !== id)
-      console.log(response);
-      console.log(response.data);
-      console.log(post);
-      this.setState({
-         post: response.data
-      })
-   }).catch((error) => console.log(error))
-  }
-  updatePost = (event, id) => {
-    // event.preventDefault()
-    // const id = event.target.id
-    axios.put(`https://womenshealth-backend.herokuapp.com/posts/${event.target.id}`, {name:this.state.name, date:this.state.date, details:this.state.details, image:this.state.image}).then((response) => {
-      console.log(response);
-      this.setState({
-         posts: response
-      })
-   }).catch(err => {
-      console.log(err);
-   })
-  }
-
-  componentDidMount() {
-    // this.getPosts()
-    axios.get('https://womenshealth-backend.herokuapp.com/posts')
-    .then((response) => {this.setState({posts: response.data})
-    // .catch((error) => console.log(error))
-})
-const posts = this.state.posts;
-}
-
-
-
-  render() {
+  render = () => {
      return(<React.Fragment>
       <div className="post">
       <h1> My Health App </h1>
       <br />
       <div className="form">
         <h3>Add New Entry</h3>
-        <form onSubmit={this.addPost}>
+        <form>
           <label htmlFor="name">Name</label> <br />
           <input type="text" id="name" onChange={this.handleChange} value={this.state.name} />
           <br />
@@ -91,7 +66,7 @@ const posts = this.state.posts;
           <label htmlFor="details">Details</label> <br />
           <input type="text" id="details" onChange={this.handleChange} value={this.state.details}/>
           <br />
-          <input className="submit" type="submit" value="Create New Post" />
+          <input className="submit" type="submit" onClick={this.addPost} value="Create New Post" />
         </form>
         </div>
         <h2> Recent Entries </h2>
@@ -108,7 +83,6 @@ const posts = this.state.posts;
                     <p>Date: {post.date}</p>
                     <button value={post.id} onClick={this.deletePost}>Delete</button>
                   </div>
-
                    <br />
                 </div>
              </div>
@@ -118,7 +92,7 @@ const posts = this.state.posts;
      </div>
      </React.Fragment>
   )
-  }
+ }
 }
 
 export default App;
