ae823c0109e9f9d45cd6001306216a403e0e965f
brittanyleonard <Belah615@gmail.com>
2021-02-24 13:02:34 -0600

    styling added

--- src/App.js
+++ src/App.js
@@ -5,106 +5,119 @@ import './App.css';
 import axios from 'axios'
 
 class App extends Component {
-  state = {
-    name: '',
-    date: '',
-    details: '',
-    image: '',
-    posts: [],
-  }
-  getPosts = () => {
-    axios.get('https://womenshealth-backend.herokuapp.com/posts')
-    .then((response) => this.setState({posts: response.data}),
-    (err) => console.error(err))
-    .catch((error) => console.error(error))
-  }
+   constructor(props) {
+      super(props)
+      this.state = {
+        posts: [],
+      }
+   }
+
+
   handleChange = (event) => {
     this.setState({
       [event.target.id]: event.target.value,
     })
   }
-  handleSubmit = (event) => {
-    event.preventDefault()
-    console.log(this.state);
-    axios.post('https://womenshealth-backend.herokuapp.com/posts', this.state).then((response) => {this.getPosts()})
-  }
-  deletePost = (event) => {
-    axios.delete('https://womenshealth-backend.herokuapp.com/posts' + event.target.value).then((response) => {this.getPosts()})
-  }
-  updatePost = (event) => {
-    event.preventDefault()
-    const id = event.target.id
-    axios.put('https://womenshealth-backend.herokuapp.com/posts' + id, this.state).then((response) => {
-      this.getPosts()
+
+ addPost = (event) => {
+    event.preventDefault();
+    axios.post('https://womenshealth-backend.herokuapp.com/posts', {
+      name: this.state.name,
+      date: this.state.date,
+      details: this.state.details,
+      image: this.state.image
+   }).then((response) => {
+      console.log(this.state.posts);
+      console.log(response.data);
       this.setState({
-         name: '',
-         date: '',
-         details: '',
-         image: '',
+         posts: response.data
       })
-    })
+   }).catch((error) => console.log(error))
+}
+
+  deletePost = (event, id) => {
+    axios.delete('https://womenshealth-backend.herokuapp.com/posts/' + event.target.value).then((response) => {
+      // this.getPosts()
+      const post = this.state.posts.filter((post) => post.id !== id)
+      console.log(response);
+      console.log(response.data);
+      console.log(post);
+      this.setState({
+         post: response.data
+      })
+   }).catch((error) => console.log(error))
   }
-  componentDidMount = () => {
-    this.getPosts()
+  updatePost = (event, id) => {
+    // event.preventDefault()
+    // const id = event.target.id
+    axios.put(`https://womenshealth-backend.herokuapp.com/posts/${event.target.id}`, {name:this.state.name, date:this.state.date, details:this.state.details, image:this.state.image}).then((response) => {
+      console.log(response);
+      this.setState({
+         posts: response
+      })
+   }).catch(err => {
+      console.log(err);
+   })
   }
-  render = () => {
-    const output = (
-      <div className="posts">
-       <h1> My Health App </h1>
-       <br />
-       <div className="form">
-         <h3>Add New Entry</h3>
-         <br />
-         <form onSubmit={this.handleSubmit}>
-           <label htmlFor="name">Name</label> <br />
-           <input type="text" id="name" onChange={this.handleChange} value={this.state.name} />
-           <br />
-           <label htmlFor="date">Date</label> <br />
-           <input type="text" id="date" onChange={this.handleChange} value={this.state.date} />
-           <br />
-           <label htmlFor="image">Image</label> <br />
-           <input type="text" id="image" onChange={this.handleChange} value={this.state.image}/>
-           <br />
-           <label htmlFor="details">Details</label> <br />
-           <input type="text" id="details" onChange={this.handleChange} value={this.state.details}/>
-           <br />
-           <input onClick={this.handleSubmit} className="submit" type="submit" value="Create New Post" />
-         </form>
-         </div>
-        {this.state.posts.map((post) => {
-          return (
-            <div className="entry">
-               <h2> Recent Entries </h2>
-              <div className="posts" key={post.id}>
-                <h3>Name: {post.name}</h3>
-                <img src={post.image} alt="image entry" />
-                <p className="details">Details: {post.details}</p>
-                <p>Date: {post.date}</p>
-                <button value={post.id} onClick={this.deletePost}>Delete</button>
-              </div>
-              <div>
-                <details>
-                  <summary>Edit Post</summary>
-                  <form id={post.id} onSubmit={this.updatePost}>
-                    <label htmlFor="name">Name</label>
-                    <br />
-                    <label htmlFor="name">Image</label>
-                    <input type="url" id="image" onChange={this.handleChange} value={this.state.image} />
-                    <br />
-                    <label htmlFor="Details">Details</label>
-                    <input type="text" id="details" onChange={this.handleChange}/>
-                    <br />
-                    <input type="submit" value="Update Post" />
-                  </form>
-                </details>
-                <br />
-              </div>
-            </div>
+
+  componentDidMount() {
+    // this.getPosts()
+    axios.get('https://womenshealth-backend.herokuapp.com/posts')
+    .then((response) => {this.setState({posts: response.data})
+    // .catch((error) => console.log(error))
+})
+const posts = this.state.posts;
+}
+
+
+
+  render() {
+     return(<React.Fragment>
+      <div className="post">
+      <h1> My Health App </h1>
+      <br />
+      <div className="form">
+        <h3>Add New Entry</h3>
+        <form onSubmit={this.addPost}>
+          <label htmlFor="name">Name</label> <br />
+          <input type="text" id="name" onChange={this.handleChange} value={this.state.name} />
+          <br />
+          <label htmlFor="date">Date</label> <br />
+          <input type="text" id="date" onChange={this.handleChange} value={this.state.date} />
+          <br />
+          <label htmlFor="image">Image</label> <br />
+          <input type="text" id="image" onChange={this.handleChange} value={this.state.image}/>
+          <br />
+          <label htmlFor="details">Details</label> <br />
+          <input type="text" id="details" onChange={this.handleChange} value={this.state.details}/>
+          <br />
+          <input className="submit" type="submit" value="Create New Post" />
+        </form>
+        </div>
+        <h2> Recent Entries </h2>
+     </div>
+     <div>
+         {this.state.posts.map((post, index) => {
+           return (
+             <div key={index}>
+             <div className="entry">
+                  <div className="posts">
+                    <h3>{post.name}</h3>
+                    <img src={post.image} alt="image entry" />
+                    <p className="details">Details: {post.details}</p>
+                    <p>Date: {post.date}</p>
+                    <button value={post.id} onClick={this.deletePost}>Delete</button>
+                  </div>
+
+                   <br />
+                </div>
+             </div>
           )
-        })}
-      </div>
-    );
-    return output;
+        })
+     }
+     </div>
+     </React.Fragment>
+  )
   }
 }
 
